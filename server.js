b7d259e984cb47e8274bae76fc92013a20b7ec2c
Douglas Leal <douglas.leal@live.com>
2023-02-26 17:01:53 -0300

    Tela chat criada

--- server.js
+++ server.js
@@ -2,8 +2,16 @@ import express from "express";
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
+app.use(express.static('public'));
+
+app.get("/", (req, res) => {
+    res.render("index");
+})
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
